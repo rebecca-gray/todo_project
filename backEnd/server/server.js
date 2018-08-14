5281c73e01bd0436cef5c8c1e722a96b25396e3a
rebecca-gray <rebecca.gray@handshake.com>
2018-08-14 19:28:34 -0400

    moved todo specific server code to separate file

--- backEnd/server/server.js
+++ backEnd/server/server.js
@@ -5,6 +5,7 @@ const corsMiddleware = require('restify-cors-middleware')
 const port = process.env.PORT || 5000;
 const moment = require('moment');
 const url = require('url');
+const Todo = require("./todo");
 const app = restify.createServer({
     name: "todoApp",
     version: "1.0.0"
@@ -34,63 +35,15 @@ const db = new sqlite3.Database('todoApp', (err) => {
     console.log('Connected to SQlite database.');
 });
 
-/**
- * @file
- * Routes:
- * GET: today, complete, incomplete, future, overdue, tomorrow, all,
- * POST:
- * PUT: id
- * DELETE: id
- *
- * sample data set:
- * stmt.run(null, 'true', 'Concert', 'La Traviata with Seamus', '2018-08-18')
- * stmt.run(null, 'false', 'Birthday Party', 'Aidan turns 9', '2018-08-23')
- * stmt.run(null, 'false', 'Extreme', 'Swim The English Channel', '2018-08-01')
- *
- * schema: {
- *     "id": {
- *         type: INTEGER PRIMARY KEY
- *      },
- *      "isComplete": {
- *         type: TEXT,
- *         "enum": [
- *            "false",
- *            "true"
- *         ]
- *         "default": "false"
- *      },
- *      "title": {
- *          "type": "string"
- *       },
- *       "body": {
- *           "type": "string"
- *       },
- *       "deadline": {
- *           "type": "string"
- *           "format": "isoDateString"
- *        }
- * }
- */
 
-db.serialize(function() {
-  db.run("CREATE TABLE if not exists todos (id INTEGER PRIMARY KEY, isComplete TEXT, title TEXT, body TEXT, deadline TEXT)");
-//   var stmt = db.prepare("INSERT INTO todos VALUES (?, ?, ?, ?, ?)");
-//    stmt.run(null, 'true', 'Concert', 'La Traviata with Seamus', '2018-08-18')
-//    stmt.run(null, 'false', 'Birthday Party', 'Aidan turns 9', '2018-08-23')
-//    stmt.run(null, 'false', 'Extreme', 'Swim The English Channel', '2018-08-01')
-//    stmt.finalize();
-
-  db.each("SELECT * FROM todos", function(err, row) {
-      console.log(row.id + ": " + row.body);
-  });
-});
+Todo.createTable(db);
 
  /**
-  * return all todo records in the db
+  * return all records in the db, short format
   * @returns {array of objects}
   */
 app.get('/all', (req, res) => {
-    const query = `SELECT id, isComplete, title, deadline FROM todos ORDER BY deadline`
+    const query = Todo.getAllQuery()
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -106,9 +59,7 @@ app.get('/all', (req, res) => {
  */
 app.get('/today', (req, res) => {
     console.log("today")
-    let today = moment().format("YYYY-MM-DD").toString()
-    const tomorrow = moment().add(1, "days").format("YYYY-MM-DD").toString()
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${today}" AND deadline <= "${tomorrow}"`
+    const query = Todo.getTodayQuery();
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -124,9 +75,7 @@ app.get('/today', (req, res) => {
  */
 app.get('/tomorrow', (req, res) => {
     console.log("tomorrow")
-    const tomorrow = moment().add(1, "days").format("YYYY-MM-DD").toString()
-    const dayAfterTomorrow = moment().add(2, "days").format("YYYY-MM-DD").toString()
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${tomorrow}" AND deadline <= "${dayAfterTomorrow}" ORDER BY deadline`
+    const query = Todo.getTomorrowQuery()
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -141,8 +90,7 @@ app.get('/tomorrow', (req, res) => {
  */
 app.get('/future', (req, res) => {
     console.log("today")
-    let today = moment().format("YYYY-MM-DD").toString()
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${today}" ORDER BY deadline`
+    const query = getFutureQuery()
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -157,9 +105,8 @@ app.get('/future', (req, res) => {
  */
 app.get('/overdue', (req, res) => {
     console.log("overdue")
-    let today = moment().format("YYYY-MM-DD").toString()
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE deadline <= "${today}" AND isComplete LIKE '%false%' ORDER BY deadline`
-    db.all(query, [], function(err, rows){
+    const query = Todo.getOverdueQuery()
+     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
         }
@@ -173,7 +120,7 @@ app.get('/overdue', (req, res) => {
  */
 app.get('/complete', (req, res) => {
     console.log("complete")
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE isComplete LIKE '%true%' ORDER BY deadline`
+    const query = Todo.getCompleteQuery();
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -188,7 +135,7 @@ app.get('/complete', (req, res) => {
  */
 app.get('/incomplete', (req, res) => {
     console.log("complete")
-    const query = `SELECT id, isComplete, title, deadline FROM todos WHERE isComplete LIKE '%false%' ORDER BY deadline`
+    const query = Todo.getIncompleteQuery()
     db.all(query, [], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -206,7 +153,7 @@ app.get('/detail', (req, res) => {
     if (!id) {
         console.log("no id sent", url.parse(req.url, true))
     }
-    const query = `SELECT * FROM todos WHERE id = ?`
+    const query = Todo.getDetailQuery(id)
     db.get(query, [id], function(err, rows){
         if (err) {
             console.log("ERR", err)
@@ -221,9 +168,11 @@ app.get('/detail', (req, res) => {
  */
 app.put('/todo', (req, res) => {
     console.log("put body", req.body)
+
     let data = [req.body.isComplete, req.body.title, req.body.body, req.body.deadline];
     let id = req.body.id;
-    db.run("UPDATE todos SET isComplete=?, title=?, body=?, deadline=? WHERE id=?",data, (err,rows) => {
+    const query = "UPDATE todos SET isComplete=?, title=?, body=?, deadline=? WHERE id=?"
+    db.run(query, data, (err,rows) => {
         if (err) {
             return console.error(err.message);
         }
---
+++ backEnd/server/todo.js
@@ -0,0 +1,133 @@
+class Todo {
+    /**
+     * create table if not exists
+     * @param {database}
+     */
+    static createTable (db) {
+        return db.serialize(() => {
+            db.run("CREATE TABLE if not exists todos (id INTEGER PRIMARY KEY, isComplete TEXT, title TEXT, body TEXT, deadline TEXT)");
+          //   var stmt = db.prepare("INSERT INTO todos VALUES (?, ?, ?, ?, ?)");
+          //    stmt.run(null, 'true', 'Concert', 'La Traviata with Seamus', '2018-08-18')
+          //    stmt.run(null, 'false', 'Birthday Party', 'Aidan turns 9', '2018-08-23')
+          //    stmt.run(null, 'false', 'Extreme', 'Swim The English Channel', '2018-08-01')
+          //    stmt.finalize();
+
+            db.each("SELECT * FROM todos", function(err, row) {
+                console.log(row.id + ": " + row.body);
+            });
+        });
+    }
+
+     /**
+     * return all records in the db, short format
+     * @returns {String}
+     */
+    static getAllQuery () {
+        return `SELECT id, isComplete, title, deadline FROM todos ORDER BY deadline`
+    }
+     /**
+     * filter records to this day
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getTodayQuery () {
+        let today = moment().format("YYYY-MM-DD").toString()
+        const tomorrow = moment().add(1, "days").format("YYYY-MM-DD").toString()
+        return `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${today}" AND deadline <= "${tomorrow}"`
+    }
+
+    /**
+     * filter records to tomorrow
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getTomorrowQuery () {
+        const tomorrow = moment().add(1, "days").format("YYYY-MM-DD").toString()
+        const dayAfterTomorrow = moment().add(2, "days").format("YYYY-MM-DD").toString()
+        return `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${tomorrow}" AND deadline <= "${dayAfterTomorrow}" ORDER BY deadline`
+    }
+
+     /**
+     * filter for records in the future
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getFutureQuery () {
+        let today = moment().format("YYYY-MM-DD").toString()
+        return `SELECT id, isComplete, title, deadline FROM todos WHERE deadline >= "${today}" ORDER BY deadline`
+    }
+
+     /**
+     * filter for records  that are overdue
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getOverdueQuery () {
+        let today = moment().format("YYYY-MM-DD").toString()
+        return `SELECT id, isComplete, title, deadline FROM todos WHERE deadline <= "${today}" AND isComplete LIKE '%false%' ORDER BY deadline`
+    }
+    /**
+     * filter for records  that are complete
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getCompleteQuery () {
+        return `SELECT id, isComplete, title, deadline FROM todos WHERE isComplete LIKE '%true%' ORDER BY deadline`
+    }
+      /**
+     * filter for records  that are incomplete
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getIncompleteQuery () {
+        `SELECT id, isComplete, title, deadline FROM todos WHERE isComplete LIKE '%false%' ORDER BY deadline`
+    }
+      /**
+     * find a single full record from an id
+     * TODO: need to handle time zones vs local time
+     * @returns {String}
+     */
+    static getDetailQuery (id) {
+        return `SELECT * FROM todos WHERE id = ?`
+    }
+};
+
+module.exports = Todo;
+
+/**
+ * @file
+ * Routes:
+ * GET: today, complete, incomplete, future, overdue, tomorrow, all,
+ * POST:
+ * PUT: id
+ * DELETE: id
+ *
+ * sample data set:
+ * stmt.run(null, 'true', 'Concert', 'La Traviata with Seamus', '2018-08-18')
+ * stmt.run(null, 'false', 'Birthday Party', 'Aidan turns 9', '2018-08-23')
+ * stmt.run(null, 'false', 'Extreme', 'Swim The English Channel', '2018-08-01')
+ *
+ * schema: {
+ *     "id": {
+ *         type: INTEGER PRIMARY KEY
+ *      },
+ *      "isComplete": {
+ *         type: TEXT,
+ *         "enum": [
+ *            "false",
+ *            "true"
+ *         ]
+ *         "default": "false"
+ *      },
+ *      "title": {
+ *          "type": "string"
+ *       },
+ *       "body": {
+ *           "type": "string"
+ *       },
+ *       "deadline": {
+ *           "type": "string"
+ *           "format": "isoDateString"
+ *        }
+ * }
+ */
